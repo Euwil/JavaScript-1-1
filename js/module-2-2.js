e914c7051e9ca112b3893b08f204e1208287d5d9
Euwil <81868357+Euwil@users.noreply.github.com>
2021-08-14 15:36:23 +0300

    Update module-2-2.js

--- js/module-2-2.js
+++ js/module-2-2.js
@@ -71,3 +71,50 @@ console.log(twoFirstelements); //[1, 2]
 let myString = "кошка умеет говорить мяу"
 let splitArray = myString.split(" ")
 console.log(splitArray); // ["кошка", "умеет", "говорить", "мяу"]
+
+let joinArray = ["кошка", "умеет", "говорить", "мяу"];
+let arrayToString = joinArray.join(" ")
+console.log(arrayToString); //кошка умеет говорить мяу
+
+//                                 ADD
+// unshift(el1, ..., el) ------------------------ push(el1, ..., el)
+//                       |        array          |
+//                       |  [0],[1],[2],[3]      |
+//               shift() ------------------------ pop() 
+//                                  DELETE
+
+let unshiftArray = [1,2,3,4,5,6]
+console.log(unshiftArray);
+let arrayElements = unshiftArray.unshift()
+console.log(arrayElements); //6 - длина массива
+arrayElements = unshiftArray.unshift(7)
+console.log(arrayElements); //7
+console.log(unshiftArray); //[7, 1, 2, 3, 4, 5, 6] добавил 7 в начало
+
+let pushArray = [7, 1, 2, 3, 4, 5, 6]
+unshiftArray.push(8)
+console.log(unshiftArray); //[7, 1, 2, 3, 4, 5, 6, 8] добавил 8 в начало
+
+let shiftArray = [7, 1, 2, 3, 4, 5, 6, 8]
+shiftArray.shift()
+console.log(shiftArray); //[1, 2, 3, 4, 5, 6, 8] убрал 7 в начале
+
+let popArray = [1, 2, 3, 4, 5, 6, 8]
+popArray.pop()
+console.log(popArray); //[1, 2, 3, 4, 5, 6] убрал 8 в конце
+
+//splice(индекс элемента, с которого начинается действие, количество элементов, которое нужно удалить) удаление
+let deleteElements = [1, 2, 3, 4, 5, 6, 8]
+deleteElements.splice(1,1)
+console.log(deleteElements); //[1, 3, 4, 5, 6, 8] удалил 1 элемент с индексом 1 - 2
+
+//splice(индекс элемента, с которого начинается действие, количество элементов, которое нужно удалить (либо 0), элементы, которые нужно добавить) добавление или замещение
+let addElements = [1, 2, 3, 4, 5, 6, 8]
+addElements.splice(0, 0, 9)
+console.log(addElements); //[9, 1, 2, 3, 4, 5, 6, 8] добавил элемент с индексом 0 - 9
+
+let firstPiece = [1,2,3]
+let secondPiece = [4,5,6]
+let thirdPiece = [7,8,9]
+let fullArray = firstPiece.concat(secondPiece, thirdPiece)
+console.log(fullArray); //[1, 2, 3, 4, 5, 6, 7, 8, 9]
