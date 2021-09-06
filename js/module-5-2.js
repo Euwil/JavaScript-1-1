d1f4ca445cfb526d2f34727b30b2dea309ca445d
Euwil <81868357+Euwil@users.noreply.github.com>
2021-09-06 18:20:13 +0300

    Update module-5-2.js

--- js/module-5-2.js
+++ js/module-5-2.js
@@ -37,5 +37,39 @@ class NewUser extends User {
     }
 }
 //создание экземпляра нового класса
-const newUser1 = new NewUser("Olha", 23, login, password)
+const newUser1 = new NewUser("Olha", 23, "Euwil", 12345)
 console.log(newUser1);
+console.log(newUser1.updateAge());
+newUser1.showLogin()
+
+//свойства-аксессоры геттер и сеттер
+class Product {
+    constructor(title, price) {
+        this.title = title
+        this._price = price
+    }
+    get price() { //getter & setter лучше называть именем свойства, с которым они будут работать
+        return this._price
+    }
+    set price(value) {
+        return this._price = value
+    }
+}
+const newProduct = new Product("banana", 25)
+console.log(newProduct.price); //get 25
+newProduct.price = 35 //set
+console.log(newProduct.price); //35
+
+//статические свойства и методы - принадлежат классу, используются для внутренней работы
+class Comment {
+    static name = "Anonim" //не отражается в экземплярах
+    static showName() {
+        console.log(Comment.name);
+    }
+    constructor(author, text) {
+       this.author = author 
+       this.text = text
+    }
+}
+const comment1 = new Comment("Euwil", "hello my dear friend")
+Comment.showName()
